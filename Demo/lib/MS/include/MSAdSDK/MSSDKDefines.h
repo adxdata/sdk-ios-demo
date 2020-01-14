c0943f96cbfdfc73a14bfdd443cecfe77f8ff486
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-01-14 18:38:28 +0800

    修复视频广告crash的bug，优化ui
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MS/include/MSAdSDK/MSSDKDefines.h
+++ Demo/lib/MS/include/MSAdSDK/MSSDKDefines.h
@@ -27,6 +27,13 @@ green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \
 blue:((float)(rgbValue & 0xFF))/255.0 \
 alpha:1.0]
 
+#define MSUIColorFromRGBA(rgbValue)\
+\
+[UIColor colorWithRed:((float)((rgbValue & 0xFF000000) >> 24))/255.0 \
+green:((float)((rgbValue & 0xFF0000) >> 26))/255.0 \
+blue:((float)(rgbValue & 0xFF00 >> 8))/255.0 \
+alpha:((float)(rgbValue & 0xFF))/255.0]
+
 #define sdkType        @"gdt"
 
 typedef NS_ENUM(NSInteger, MSShowType) {
