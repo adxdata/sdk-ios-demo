7aea00c237a3247e3a35bd8acec2d2b6488a711d
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-02-29 23:50:03 +0800

    去掉重播按钮
    webview广告跳转appstore或其他app时，关闭webview界面
    
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.5";
-static NSInteger kMSVersionCode = 30;
+static NSString *kMSVersionName = @"0.8.7";
+static NSInteger kMSVersionCode = 32;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
