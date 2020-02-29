8b953b995380273b4302078298c1f52fdfef60b6
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-02-29 23:12:30 +0800

    webview广告跳转appstore或其他app时，关闭webview界面
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.6";
-static NSInteger kMSVersionCode = 31;
+static NSString *kMSVersionName = @"0.8.7";
+static NSInteger kMSVersionCode = 32;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
