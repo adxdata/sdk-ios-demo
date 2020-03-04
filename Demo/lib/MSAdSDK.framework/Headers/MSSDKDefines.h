f99311d779d101d35eeb035334e291031bd00773
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-03-04 14:16:09 +0800

    增加广告logo
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.8";
-static NSInteger kMSVersionCode = 33;
+static NSString *kMSVersionName = @"0.8.9";
+static NSInteger kMSVersionCode = 34;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
