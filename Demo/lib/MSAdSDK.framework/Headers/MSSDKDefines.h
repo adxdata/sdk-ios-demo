ad49b71720b3798539fb400fbeb3a3184f1eda45
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-03-09 15:18:30 +0800

    修复视频信息流详情界面无声的bug
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.12";
-static NSInteger kMSVersionCode = 37;
+static NSString *kMSVersionName = @"0.8.13";
+static NSInteger kMSVersionCode = 38;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
