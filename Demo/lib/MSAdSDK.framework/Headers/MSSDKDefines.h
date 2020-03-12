50d72b1e7ac498bd44b9c342afcfde8a8a972423
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-03-13 02:10:59 +0800

    [0.8.18][feature]增加deeplink时判断是否安装应用
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.17";
-static NSInteger kMSVersionCode = 42;
+static NSString *kMSVersionName = @"0.8.18";
+static NSInteger kMSVersionCode = 43;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
