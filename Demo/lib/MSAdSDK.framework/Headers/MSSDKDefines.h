b8635857b5ab9c9c94872864c15f6836751f9df9
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-03-27 00:55:29 +0800

    [0.8.32][bugfix] 修复deeplink跳转时，webview白屏不会被关掉的bug
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.29";
-static NSInteger kMSVersionCode = 54;
+static NSString *kMSVersionName = @"0.8.32";
+static NSInteger kMSVersionCode = 57;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
