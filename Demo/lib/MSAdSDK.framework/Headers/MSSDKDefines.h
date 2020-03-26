2e876775fa5dadd3a95c87d176c6a6c315c6a2ce
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-03-26 17:46:47 +0800

    [0.8.31][bugfix] 修复广点通自渲染1.0显示和回调问题
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.30";
-static NSInteger kMSVersionCode = 55;
+static NSString *kMSVersionName = @"0.8.31";
+static NSInteger kMSVersionCode = 56;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
