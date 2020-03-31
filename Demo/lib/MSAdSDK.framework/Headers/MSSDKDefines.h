b26a8ad9f17ad0d19ae7cf69bfb64027ccd18b69
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-03-31 14:30:20 +0800

    [0.8.35][bugfix] 开屏广告没有广告数据时没有回调的bug [0.8.34][bugfix] 修复偶尔开屏会崩溃的bug
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.33";
-static NSInteger kMSVersionCode = 58;
+static NSString *kMSVersionName = @"0.8.35";
+static NSInteger kMSVersionCode = 60;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
