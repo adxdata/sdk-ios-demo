38431c3f43c1c07e9c468448789252a817b5ae5a
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-04-01 13:15:38 +0800

    [0.8.37][bugfix] 删除bundle的CFBundleExecutable
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.36";
-static NSInteger kMSVersionCode = 61;
+static NSString *kMSVersionName = @"0.8.37";
+static NSInteger kMSVersionCode = 62;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
