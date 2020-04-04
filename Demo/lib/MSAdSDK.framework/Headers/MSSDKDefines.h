12f1cd765c3c03a62c486937fded9838e7b90b4c
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-04-04 18:10:31 +0800

    [0.8.42][bugfix] 修复点击广告后偶现黑屏的bug
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.41";
-static NSInteger kMSVersionCode = 66;
+static NSString *kMSVersionName = @"0.8.42";
+static NSInteger kMSVersionCode = 67;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
