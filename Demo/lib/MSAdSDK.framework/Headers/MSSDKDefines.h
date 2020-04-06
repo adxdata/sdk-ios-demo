5771b83861715b68b404395182c4ea351b1396b7
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-04-07 00:45:56 +0800

    [0.8.45][feature] 移除Masonry
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.41";
-static NSInteger kMSVersionCode = 66;
+static NSString *kMSVersionName = @"0.8.45";
+static NSInteger kMSVersionCode = 70;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
