2b0de87c03065f42e4958a50932723ddb0b3737e
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-04-07 00:45:56 +0800

    [0.8.45][feature] 移除Masonry
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/MSAds/MSVideoAd/VideoViewController.m
+++ Demo/MSAds/MSVideoAd/VideoViewController.m
@@ -9,7 +9,6 @@
 #import "VideoViewController.h"
 #import <MSAdSDK/MSAdSDK.h>
 #import "IdProviderFactory.h"
-#import "Masonry.h"
 
 @interface VideoViewController () <MSVideoAdDelegate>
 
@@ -79,55 +78,36 @@
 }
 
 - (void)initPauseButton {
-    self.pauseButton = [[UIButton alloc] init];
-    [self.pauseButton setTitle:@"暂停" forState:UIControlStateNormal];
-    [self.view addSubview:self.pauseButton];
-    [self.pauseButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    self.pauseButton.hidden = YES;
-    [self.pauseButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.centerX.equalTo(self.view.mas_centerX);
-        make.top.equalTo(self.container.mas_bottom);
-        make.size.mas_equalTo(CGSizeMake(80, 40));
-    }];
+    self.pauseButton = [self createButton:@"暂停" rect:CGRectMake((msScreenWidth - 80)/2, 310, 80, 30)];
     [self.pauseButton addTarget:self action:@selector(playOrPause:) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)initMuteButton {
-    self.muteButton = [[UIButton alloc] init];
-    [self.muteButton setTitle:@"静音" forState:UIControlStateNormal];
-    [self.muteButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [self.view addSubview:self.muteButton];
-    [self.muteButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.pauseButton.mas_bottom);
-        make.centerX.equalTo(self.view.mas_centerX);
-        make.size.mas_equalTo(CGSizeMake(80, 40));
-    }];
+    self.muteButton = [self createButton:@"静音" rect:CGRectMake((msScreenWidth - 80)/2, 350, 80, 30)];
     [self.muteButton addTarget:self action:@selector(muteOrUnmute:) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)initSizeButton {
-    self.sizeButton = [[UIButton alloc] init];
-    [self.sizeButton setTitle:@"修改大小" forState:UIControlStateNormal];
-    [self.sizeButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [self.view addSubview:self.sizeButton];
-    [self.sizeButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.muteButton.mas_bottom);
-        make.centerX.equalTo(self.view.mas_centerX);
-        make.size.mas_equalTo(CGSizeMake(80, 40));
-    }];
+    self.sizeButton = [self createButton:@"修改大小" rect:CGRectMake((msScreenWidth - 80)/2, 390, 80, 30)];
     [self.sizeButton addTarget:self action:@selector(updateSize:) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)initTimeButton {
-    self.timeButton = [[UIButton alloc] init];
-    [self.view addSubview:self.timeButton];
-    [self.timeButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-    [self.timeButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.sizeButton.mas_bottom);
-        make.centerX.equalTo(self.view.mas_centerX);
-        make.size.mas_equalTo(CGSizeMake(200, 40));
-    }];
+    self.timeButton = [self createButton:@"" rect:CGRectMake((msScreenWidth - 200)/2, 430, 200, 30)];
+    self.timeButton.hidden = YES;
     [self.timeButton addTarget:self action:@selector(showTime:) forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (UIButton *)createButton:(NSString *)text rect:(CGRect)rect{
+    UIButton *button = [[UIButton alloc] initWithFrame:rect];
+    [button setTitle:text forState:UIControlStateNormal];
+    [self.view addSubview:button];
+    button.titleLabel.font = [UIFont systemFontOfSize:14];
+    [button setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    button.layer.cornerRadius = 5;
+    button.backgroundColor = MSUIColorFromRGB(0x46A3FF);
+    [button setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+    return button;
 }
 
 - (void)loadAd {
@@ -161,6 +141,7 @@
     NSInteger duration = [self.videoAd duration];
     NSString *text = [NSString stringWithFormat:@"当前播放时间（秒）:%ld/%ld", time, duration];
     [self.timeButton setTitle:text forState:UIControlStateNormal];
+    self.timeButton.hidden = NO;
     if (time >= duration) {
         [self destroyTimer];
     }
