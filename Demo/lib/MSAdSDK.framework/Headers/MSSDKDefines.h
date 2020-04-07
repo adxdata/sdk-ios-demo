9e724c6249678a76cacc9740f89b5218a8569651
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-04-08 02:26:05 +0800

    0.8.46][feature] 移除Reachability
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.45";
-static NSInteger kMSVersionCode = 70;
+static NSString *kMSVersionName = @"0.8.46";
+static NSInteger kMSVersionCode = 71;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
