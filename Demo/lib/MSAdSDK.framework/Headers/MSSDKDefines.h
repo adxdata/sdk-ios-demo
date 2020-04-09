30be35195ba800ebce3d84d6186415aa87e87413
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-04-09 18:15:41 +0800

    [0.8.47][bug]修复美数信息流点击回调不触发的bug
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.46";
-static NSInteger kMSVersionCode = 71;
+static NSString *kMSVersionName = @"0.8.47";
+static NSInteger kMSVersionCode = 72;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
