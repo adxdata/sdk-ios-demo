a85589997315b4daf358c14b238d2c8d22b7d839
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-04-13 16:41:26 +0800

    Revert "增加切换广告平台的ui"
    This reverts commit b86a89228b487ab34bcb4d43f6d156df89d05b96.
    
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/MSAdViewController.m
+++ Demo/MSAdViewController.m
@@ -11,19 +11,13 @@
 #import <objc/runtime.h>
 #import "AlterBaseUrlView.h"
 #import <CoreLocation/CoreLocation.h>
-#import <MSAdSDK/MSAdSDK.h>
-#import "IdProviderFactory.h"
-
-#import <BUAdSDK/BUAdSDKManager.h>
-#import <BaiduMobAdSDK/BaiduMobAdCommonConfig.h>
-#import "GDTSDKConfig.h"
 
 @interface MSAdViewController () <UITableViewDelegate, UITableViewDataSource>
+
 
 @property (nonatomic, strong) UITableView *tableView;
 @property (nonatomic, strong) AlterBaseUrlView* header;
 @property (nonatomic, strong) CLLocationManager *locationManager;
-@property (nonatomic, strong) NSMutableArray<UIButton *> *buttons;
 
 @end
 
@@ -31,9 +25,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = UIColor.whiteColor;
-    [self setupPlatformButtons];
     [self.view addSubview:self.tableView];
+    self.tableView.frame = self.view.bounds;
 
     _locationManager = [[CLLocationManager alloc] init];
     CLAuthorizationStatus CLstatus = [CLLocationManager authorizationStatus];
@@ -41,57 +34,6 @@
     if (CLstatus == kCLAuthorizationStatusNotDetermined) {
         [_locationManager requestAlwaysAuthorization];
         [_locationManager requestWhenInUseAuthorization];
-    }
-}
-
-- (void)setupPlatformButtons {
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, msScreenWidth, 60)];
-    view.backgroundColor = UIColor.whiteColor;
-    [self.view addSubview:view];
-    UIView *border = [[UIView alloc] initWithFrame:CGRectMake(0, 59, msScreenWidth, 1)];
-    border.backgroundColor = MSUIColorFromRGB(0xE0E0E0);
-    [self.view addSubview:border];
-
-    self.buttons = [[NSMutableArray alloc] init];
-    CGFloat btnMargin = 10;
-    CGFloat btnWidth = (msScreenWidth - btnMargin * 5) / 4;
-    CGFloat btnHeight = 30;
-    CGFloat btnY = 15;
-
-    NSString *msStr = [NSString stringWithFormat:@"美数%@", [MSAdSDK getVersionName]];
-    NSString *csjStr = [NSString stringWithFormat:@"穿山甲%@", [BUAdSDKManager SDKVersion]];
-    NSString *gdtStr = [NSString stringWithFormat:@"广点通%@", [GDTSDKConfig sdkVersion]];
-    NSString *bdStr = [NSString stringWithFormat:@"百度%@", SDK_VERSION_IN_MSSP];
-
-    [self.buttons addObject:[self createButton:msStr rect:CGRectMake(btnMargin, btnY, btnWidth, btnHeight)]];
-    [self.buttons addObject:[self createButton:csjStr rect:CGRectMake(btnMargin + 1 * (btnMargin + btnWidth), btnY, btnWidth, btnHeight)]];
-    [self.buttons addObject:[self createButton:gdtStr rect:CGRectMake(btnMargin + 2 * (btnMargin + btnWidth), btnY, btnWidth, btnHeight)]];
-    [self.buttons addObject:[self createButton:bdStr rect:CGRectMake(btnMargin + 3 * (btnMargin + btnWidth), btnY, btnWidth, btnHeight)]];
-
-    for (UIButton *btn in self.buttons) {
-        [btn addTarget:self action:@selector(platformSelected:) forControlEvents:UIControlEventTouchUpInside];
-    }
-    [self platformSelected:self.buttons[0]];
-}
-
-- (void)platformSelected:(UIButton *)btn {
-    for (UIButton *tmp in self.buttons) {
-        if (tmp == btn) {
-            tmp.selected = YES;
-            tmp.backgroundColor = MSUIColorFromRGB(0x46A3FF);
-            if ([btn.currentTitle containsString:@"美数"]) {
-                [[IdProviderFactory sharedIdProviderFactory] setDefaultPlatform:PlatformNameMS];
-            } else if ([btn.currentTitle containsString:@"穿山甲"]) {
-                [[IdProviderFactory sharedIdProviderFactory] setDefaultPlatform:PlatformNameCSJ];
-            } else if ([btn.currentTitle containsString:@"广点通"]) {
-                [[IdProviderFactory sharedIdProviderFactory] setDefaultPlatform:PlatformNameGDT];
-            } else if ([btn.currentTitle containsString:@"百度"]) {
-                [[IdProviderFactory sharedIdProviderFactory] setDefaultPlatform:PlatformNameBD];
-            }
-        } else {
-            tmp.selected = NO;
-            tmp.backgroundColor = UIColor.whiteColor;
-        }
     }
 }
 
@@ -134,7 +76,7 @@
 - (UITableView *)tableView
 {
     if (!_tableView) {
-        _tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 60, msScreenWidth, msScreenHeight - 60)];
+        _tableView = [[UITableView alloc] init];
         _tableView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
         _tableView.delegate = self;
         _tableView.dataSource = self;
@@ -148,19 +90,6 @@
         _header = [[AlterBaseUrlView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 170)];
     }
     return _header;
-}
-
-- (UIButton *)createButton:(NSString *)text rect:(CGRect)rect {
-    UIButton *button = [[UIButton alloc] initWithFrame:rect];
-    [self.view addSubview:button];
-    button.layer.cornerRadius = 5;
-    button.titleLabel.font = [UIFont systemFontOfSize:12];
-    [button setTitle:text forState:UIControlStateNormal];
-    [button setTitleColor:UIColor.blackColor forState:UIControlStateNormal];
-    [button setTitleColor:UIColor.whiteColor forState:UIControlStateSelected];
-    button.layer.borderColor = [MSUIColorFromRGB(0xADADAD) CGColor];
-    button.layer.borderWidth = 1;
-    return button;
 }
 
 @end
