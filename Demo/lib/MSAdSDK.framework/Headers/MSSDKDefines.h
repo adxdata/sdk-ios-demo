9a12138f97f294d5dc4382edb8f316079fbe97fa
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-04-13 22:12:18 +0800

    [0.8.55][feature] 扩大激励视频关闭按钮的点击区域
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.54";
-static NSInteger kMSVersionCode = 79;
+static NSString *kMSVersionName = @"0.8.55";
+static NSInteger kMSVersionCode = 80;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
