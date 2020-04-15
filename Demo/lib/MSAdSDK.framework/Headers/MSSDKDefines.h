6f4914d6a4960fe6e5af7a19c5990a1ba55d8400
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-04-15 20:52:56 +0800

    更新版本号至[0.8.58][83]
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSSDKDefines.h
@@ -11,8 +11,8 @@
 #import "MSConfiger.h"
 #import "MSCommCore.h"
 
-static NSString *kMSVersionName = @"0.8.57";
-static NSInteger kMSVersionCode = 82;
+static NSString *kMSVersionName = @"0.8.58";
+static NSInteger kMSVersionCode = 83;
 
 //是否是竖屏
 #define IsPortrait ([[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortrait || [[UIApplication sharedApplication] statusBarOrientation] == UIInterfaceOrientationPortraitUpsideDown)
