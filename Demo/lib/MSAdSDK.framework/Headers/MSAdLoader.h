490d0abdf477d52213d6c1e08a00a575fb358a7e
zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>
2020-05-29 17:04:20 +0800

    [0.8.70] 移除自渲染信息流中广点通相关代码
    修复请求返回204时，延迟cancel依然会回调error的bug
    
    Signed-off-by: zhouzhiqiang <4-zhouzhiqiang@users.noreply.git.adxdata.com>

--- Demo/lib/MSAdSDK.framework/Headers/MSAdLoader.h
+++ Demo/lib/MSAdSDK.framework/Headers/MSAdLoader.h
@@ -37,6 +37,12 @@
 /** 广告是否被取消显示 */
 @property (nonatomic, assign) BOOL canceled;
 
+/**
+ *  拉取广告超时时间，默认为3秒
+ *  详解：拉取广告超时时间，超过此时间则放弃此次广告展示机会。
+ */
+@property (nonatomic, assign) NSInteger fetchDelay;
+
 /*
  * 访问美数服务器，加载广告数据
  */
